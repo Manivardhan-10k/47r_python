ffaea8a60023b72183e543ce14ea6572c3568338
Manivardhan-10k <manivardhan.10kcoders@gmail.com>
2025-07-22 15:49:49 +0530

    day_41

---
+++ day_41/ts.py
@@ -0,0 +1,77 @@
+#strings 
+#numbers 
+#lists 
+#dictionaries 
+#tuples 
+#sets 
+
+
+#it is a ordered collection datatype 
+#() 
+#it is immutable
+
+t=(1,2,3,4,5,6,6,7,8,10,1)
+# print(len(t))
+# for i in t:
+#     print(i)
+# print(t.count(60))
+
+#index 
+# print(t.index(1,1,9)) 
+
+
+#SET 
+
+# set1={"hello"}
+# print(type(set1))
+
+# set1={1,1,2,3,2,3,54,5,5,8,55,"hello"}
+# print(set1) 
+
+# setA={2,3,4,5}
+# setB={3,4,5,6,7,8,9,10,0} 
+# setA.add(True)
+# setA.add(0) 
+# setA.add(False) 
+# print(setA)
+# setB.add(True)
+# setB.add(False)
+# print(setB) 
+
+# num_set=set()# to initialise a empty set
+# num_set.update((1,2))
+# num_set.update("hello")
+# num_set.update(["how","are","you"])
+# print(num_set)
+
+num_set={2,3,4,6,1,0}
+# num_set={"hello","h","i","are","you","hi",True}
+# num_set.remove(60)
+# print(num_set)
+# num_set.discard(6)
+# print(num_set)
+# print(num_set)
+# print(num_set.pop())
+# num_set.clear() 
+# print(num_set) 
+
+# set1={1,2,3}
+# set2={6,7,8,9,10,1,2,3} 
+
+# combined=set2.union(set1) 
+# print(combined)
+
+# print(set2.intersection(set1)) 
+# print(True==1)
+# print(set2.intersection(set1))  
+
+# set1={5,6,7}
+# set2={1,2,3,4}
+# print(set2.difference(set1))
+# print(set2.symmetric_difference(set1))
+# print(set2.issubset(set1)) 
+# print(set1.issuperset(set2)) 
+# print(set1.isdisjoint(set2))
+# set3=set1.copy()
+# print(set3)
+
